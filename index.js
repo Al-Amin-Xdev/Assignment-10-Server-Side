36495683cf27b2f0a8aabf9b0cf0c177ea8df8bf
Md. Al-Amin Mia <md.alamin.mia.webdev@gmail.com>
2025-11-09 10:38:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get('/', (req, res) => {
+  res.send('Backend server running ✅');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
