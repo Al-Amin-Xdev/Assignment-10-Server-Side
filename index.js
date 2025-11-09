2c7c1b351093dbeb0dc305c7cbf42754c9bb0db0
Md. Al-Amin Mia <md.alamin.mia.webdev@gmail.com>
2025-11-09 23:26:57 +0600

    3rd

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 require('dotenv').config();
 
 const app = express();
@@ -9,10 +11,62 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1rbama4.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 // test route
 app.get('/', (req, res) => {
   res.send('Backend server running ✅');
 });
+
+async function run() {
+  try {
+
+    await client.connect();
+
+    const ProductData = client.db('Krishi-Link');
+    const ProductCollection = ProductData.collection('All-Crops-Details');
+
+    
+    // Get All Crops Data
+
+    app.get('/allproducts', async(req, res)=>{
+
+      try {
+      const cursor = ProductCollection.find();
+      const result = await cursor.toArray();
+      console.log(result);
+      res.send(result);
+
+      }
+      catch(error){
+        console.log(error);
+        res.status(500).send({ message: "Failed to fetch data" });
+
+      }
+    })
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
