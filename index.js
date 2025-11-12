6209233eab2e7f27f974cc062f718ff525266b7e
Md. Al-Amin Mia <md.alamin.mia.webdev@gmail.com>
2025-11-12 22:57:03 +0600

    final commit

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./back-end-authentication-70d55-firebase-adminsdk-fbsvc-99bd52afcd.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
 require('dotenv').config();
 
 const app = express();
@@ -10,6 +18,43 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+
+// Token verification with firebase Token and admin 
+const VerifyFirebaseToken = async(req, res, next)=>{
+  
+  const token = req.headers.authorization;
+  console.log(token);
+
+
+  if(!token){
+    return res.status(401).send({message: "Forbidden"})
+  }
+
+  const tokenn = token.split(" ")[1];
+
+  if(!tokenn){
+    return res.status(401).send({message: "Forbidden"})
+  }
+
+  // Verify token with firebse admin
+
+  try{
+
+    const decode = await admin.auth().verifyIdToken(tokenn);
+    next();
+  }
+  catch{
+
+    return res.status(401).send({message:"forbidden"})
+
+  }
+}
+
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1rbama4.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -87,7 +132,7 @@ async function run() {
 
     // Updating interest
 
-    app.patch('/allproducts/:id', async(req, res)=>{
+    app.patch('/allproducts/:id', async(req,VerifyFirebaseToken, res)=>{
       const id = req.params.id;
       const updateInterest = req.body;
       const query = {_id: new ObjectId(id)};
